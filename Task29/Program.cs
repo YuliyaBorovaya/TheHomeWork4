27d605cbcdb04e6623cdc781d4ecc721c4fa008e
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-08-30 23:22:36 +0300

    Вывод массива

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+int[] array1 = {1, 2, 5, 7, 19};
+int[] array2 = {6, 1, 33};
+
+int[] result = new int[array1.Length + array2.Length];
+array1.CopyTo(result, 0);
+array2.CopyTo(result, array1.Length);
+ 
+Console.WriteLine(String.Join(", ", result));    
+    
+
+
